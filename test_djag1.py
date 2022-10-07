0d7c50073d5a05357c6755049d71c40a1b979653
Rob4ik15 <114750022+Rob4ik15@users.noreply.github.com>
2022-10-07 19:36:51 +0300

    Add files via upload

--- test_djag1.py
+++ test_djag1.py
@@ -1,5 +1,11 @@
-a = (input("напиши потато "))
-b = int(0)
-for i in range (len (a)):
-    b += int(a [i]) * 2 **(len (a) -i -1)
-print (b)
+a = [1, 2 ,3,4]
+for i in range (len (a)-1):
+    print(a)
+    count = 0
+    for f in range (len(a)-i-1):
+        if a[i]>a[i+1]:
+            a[i],a[i+1] = a[i+1],a[i];
+            count += 1
+    
+    if count == 0:
+        break
